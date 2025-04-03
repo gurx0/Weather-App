8866262ddcefeaa29f5200a52604efabaf8a36b7
gurx0 <pankratov20062504@gmail.com>
2025-04-03 15:43:47 +0300

    add details screen style

--- app/src/main/java/com/example/myapplication/Screens/DetailsScreen.kt
+++ app/src/main/java/com/example/myapplication/Screens/DetailsScreen.kt
@@ -1,15 +1,19 @@
 package com.example.myapplication.Composable.Screens
 
-
 import android.util.Log
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
@@ -18,22 +22,27 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import com.example.myapplication.VM.DetailsViewModel
 import com.example.myapplication.VM.WeatherState
 import com.example.myapplication.network.ForecastItem
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 @Composable
 fun DetailsScreen(
     navController: NavController,
     viewModel: DetailsViewModel = hiltViewModel(),
-    ) {
+) {
     val weatherState = viewModel.weatherState.value
     val lat = 55.7558
     val lon = 37.6173
@@ -46,10 +55,10 @@ fun DetailsScreen(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
     ) {
-        Box {
+        Box(contentAlignment = Alignment.Center) {
             when (weatherState) {
                 is WeatherState.Loading -> {
-                    CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+                    CircularProgressIndicator()
                 }
                 is WeatherState.Success -> {
                     val forecast = weatherState.data.list
@@ -63,7 +72,8 @@ fun DetailsScreen(
                     Text(
                         text = weatherState.message,
                         color = Color.Red,
-                        modifier = Modifier
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.fillMaxWidth()
                     )
                 }
             }
@@ -73,25 +83,67 @@ fun DetailsScreen(
 
 @Composable
 fun WeatherItem(forecast: ForecastItem) {
+    val formattedTime = remember(forecast.dt_txt) {
+        val inputFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+        val outputFormat = SimpleDateFormat("LLLL d HH:mm", Locale("ru"))
+        try {
+            inputFormat.parse(forecast.dt_txt)?.let { outputFormat.format(it) } ?: forecast.dt_txt
+        } catch (e: Exception) {
+            forecast.dt_txt
+        }
+    }
+
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(8.dp),
+            .padding(8.dp)
+            .background(Color.White.copy(alpha = 0.2f), shape = RoundedCornerShape(16.dp))
+            .border(BorderStroke(2.dp, Color.White), shape = RoundedCornerShape(16.dp)),
         elevation = CardDefaults.cardElevation(4.dp)
     ) {
-        Column(modifier = Modifier.padding(16.dp)) {
-            Text(text = "Время: ${forecast.dt_txt}", style = MaterialTheme.typography.bodyMedium)
-            Text(text = "Температура: ${forecast.main.temperature}°C", style = MaterialTheme.typography.bodyMedium)
-            Text(text = "Погода: ${forecast.weather[0].description}", style = MaterialTheme.typography.bodyMedium)
-            Text(text = "Влажность: ${forecast.main.humidity}%", style = MaterialTheme.typography.bodyMedium)
-
+        Row(Modifier.fillMaxSize()) {
+            Column(
+                modifier = Modifier.padding(16.dp),
+                horizontalAlignment = Alignment.Start
+            ) {
+                Text(
+                    text = "Время: $formattedTime",
+                    style = MaterialTheme.typography.bodyMedium,
+                    textAlign = TextAlign.Start,
+                    fontSize = 18.sp,
+                    color = Color.White
+                )
+                Text(
+                    text = "Температура: ${forecast.main.temperature}°C",
+                    style = MaterialTheme.typography.bodyMedium,
+                    textAlign = TextAlign.Start,
+                    fontSize = 18.sp,
+                    color = Color.White
+                )
+                Text(
+                    text = "Погода: ${forecast.weather[0].description}",
+                    style = MaterialTheme.typography.bodyMedium,
+                    textAlign = TextAlign.Start,
+                    fontSize = 18.sp,
+                    color = Color.White
+                )
+                Text(
+                    text = "Влажность: ${forecast.main.humidity}%",
+                    style = MaterialTheme.typography.bodyMedium,
+                    textAlign = TextAlign.Start,
+                    fontSize = 18.sp,
+                    color = Color.White
+                )
+            }
             val iconUrl = "http://openweathermap.org/img/wn/${forecast.weather[0].icon}.png"
             Log.d("WeatherItem", "Loading image: $iconUrl")
 
             AsyncImage(
                 model = iconUrl,
                 contentDescription = "Weather icon",
-                modifier = Modifier.size(50.dp)
+                modifier = Modifier
+                    .size(50.dp)
+                    .padding(top = 8.dp)
             )
         }
     }
