5c3db9937cdbbfcec358bdbfebad9242bcc6dd30
gurx0 <pankratov20062504@gmail.com>
2025-04-05 22:21:27 +0300

    add descriptions

--- app/src/main/java/com/example/myapplication/app/App.kt
+++ app/src/main/java/com/example/myapplication/app/App.kt
@@ -3,5 +3,7 @@ package com.example.myapplication.app
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
+// инициализация андроид приложения
+
 @HiltAndroidApp
 class App: Application()
